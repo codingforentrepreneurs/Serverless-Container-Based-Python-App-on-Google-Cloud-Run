0a6634e4de88d0324d3687ed4374cd471c936439
CodingForEntrepreneurs <hello@teamcfe.com>
2023-02-10 15:03:30 -0600

    10 - Test Code using FastAPI and PyTest

---
+++ src/tests.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+
+client = TestClient(app)
+
+def test_get_home_status():
+    path = "/"
+    # python requests
+    # r = requests.get(path)
+    response = client.get(path)
+    status_code = response.status_code
+    content_type = response.headers['content-type']
+    assert status_code == 201 # HTTP response
+    assert content_type == "application/json"
